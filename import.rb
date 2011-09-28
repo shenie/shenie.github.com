860ab555e5565d58bf182a8b0e3424200b16b7c6
Andy Shen <andy@shenie.net>
2011-09-28 19:25:37 +1000

    script to import tumblr

---
+++ import.rb
@@ -0,0 +1,38 @@
+#!/usr/bin/env ruby
+
+require 'rubygems'
+require 'nokogiri'
+
+f = File.open("#{ENV['HOME']}/Dropbox/tumblr_shenie.xml")
+doc = Nokogiri::XML(f)
+f.close
+
+items = doc.css('item')
+
+puts "#{items.size} items"
+
+doc.css('item').each do |item|
+  title = item.css('title').text
+  content = item.css('content').text
+  post_date = item.css('post_date').text[0..-4]
+
+
+  post = <<END
+---
+layout: post
+title: "#{title}"
+date: #{post_date}
+comments: true
+categories: 
+---
+
+#{content}
+END
+
+filename = "source/_posts/#{post_date.split.first}-#{title.gsub(/[,&'"_~;#:)@*%$]/, '').split.join('-').downcase[0..80]}.markdown"
+  File.open(filename, 'w') do |f|
+    f.puts post
+  end
+
+end
+
